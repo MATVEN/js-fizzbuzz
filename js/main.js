8e39d524c191af43b38af133050760997bc847d0
MATVEN <mattiavena2993@gmail.com>
2019-10-25 09:27:30 +0200

    stamp

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 // variabili
 var maxNumber = 100;
-
+var stamp = document.getElementById('box')
 // ciclo
 for (var i = 1; i <= maxNumber; i++) {
-  if (i % 3 ===0 && i % 5 ===0) {
-   console.log('FizzBuzz');
+  if (i % 3 === 0 && i % 5 === 0) {
+    stamp.innertHTML += 'FizzBuzz';
+
  }
-  else if (i % 3 ===0) {
-    console.log('Fizz');
+  else if (i % 3 === 0) {
+    stamp.innertHTML +='Fizz';
  }
-  else if (i % 5 ===0) {
-    console.log('Buzz');
+  else if (i % 5 === 0) {
+    stamp.innertHTML +='Buzz';
  }
   else {
-    console.log(i);
+      stamp.innertHTML += i;
  }
 }
